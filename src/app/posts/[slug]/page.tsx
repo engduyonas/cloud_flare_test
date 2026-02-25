93e6a404acdbcd14f73554a90122fe6295b4a67b
yonas <engdiyonas1094@gmail.com>
2026-02-25 12:44:27 +0300

    l

--- src/app/posts/[slug]/page.tsx
+++ src/app/posts/[slug]/page.tsx
@@ -63,6 +63,8 @@ export async function generateMetadata(props: Params): Promise<Metadata> {
   };
 }
 
+export const dynamicParams = false;
+
 export async function generateStaticParams() {
   const posts = getAllPosts();
 
